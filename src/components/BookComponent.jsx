447798fe85b93af5a98ca2d9d250adb8011e7f01
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2023-12-23 10:31:52 -0800

    Correcciones Book

--- src/components/BookComponent.jsx
+++ src/components/BookComponent.jsx
@@ -3,27 +3,35 @@ import Calendar from 'react-calendar'
 import { useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import 'react-time-picker/dist/TimePicker.css';
-
 import TimePicker from 'react-time-picker';
 import '../styles/Sample.css';
+import 'react-clock/dist/Clock.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import moment from 'moment'
 
 export const BookComponent = () => {
-    const [day, setValue] = useState(new Date());
     const [hour, onHour] = useState('11:00');
-    const horaReserva = hour;
-    const fechaReserva = day;
-    let fechaReservaString = fechaReserva.toString();
-    
-    function onChange(nextValue) {
-        setValue(nextValue);
+    // const horaReserva = hour;
+    const [dateState, setDateState] = useState(new Date().toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" }));
+
+    const changeDate = (e) => {
+        setDateState(e)
     }
 
     return (
         <>
-            <Calendar onChange={onChange} value={day} />
-            <TimePicker onChange={onHour} value={hour} />
-            <p>{horaReserva}</p>
-            <p>{fechaReservaString}</p>
+            <Container>
+                <Row className="justify-content-md-center m-5">
+                    <Col md="auto" className="justify-content-md-center m-5">
+                        <Calendar value={dateState} onChange={changeDate} />
+                    </Col>
+                    <Col md="auto" className="justify-content-md-center m-5">
+                        <h4>Hora Seleccionada: {hour}</h4>
+                        <h4>Fecha Seleccionada: <b>{moment(dateState).format('MMMM Do YYYY')}</b></h4>
+                        <TimePicker onChange={onHour} value={hour} />
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
