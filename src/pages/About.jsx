f0f7e204ae42dc02245f88e339a872ad945eb7dd
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2023-12-23 22:24:18 -0800

    Corrección margenes Home, y implementación About Us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,45 @@ import '../styles/styles.css'
 export const About = () => {
   return (
     <>
+      <section>
+        <ul>
+          <li>
+            <div className='content'>
+              <span>Cocina</span>
+              <h2>Personal</h2>
+              <p>Preocupados por dar un buen servicio.</p>
+            </div>
+          </li>
+          <li>
+            <div className='content'>
+            <span>Cocina</span>
+              <h2>Personal</h2>
+              <p>Preocupados por dar un buen servicio.</p>
+            </div>
+          </li>
+          <li>
+            <dir className='content'>
+            <span>Cocina</span>
+              <h2>Personal</h2>
+              <p>Preocupados por dar un buen servicio.</p>
+            </dir>
+          </li>
+          <li>
+            <dir className='content'>
+            <span>Cocina</span>
+              <h2>Personal</h2>
+              <p>Preocupados por dar un buen servicio.</p>
+            </dir>
+          </li>
+          <li>
+            <dir className='content'>
+            <span>Cocina</span>
+              <h2>Personal</h2>
+              <p>Preocupados por dar un buen servicio.</p>
+            </dir>
+          </li>
+        </ul>
+      </section>
     </>
   )
 }
